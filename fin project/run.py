bd38dea68d18d4d54c810fa7fb71e915a0eafa57
tunguyen17 <tanguyen17@M1611118.local>
2016-11-10 15:02:29 -0500

    student ins and del
    Student insertion and deletion work.

--- fin project/run.py
+++ fin project/run.py
@@ -2,6 +2,8 @@ import sqlite3
 import logging
 
 class database(object):
+    #constructor for the database object
+    #the input is a database
     def __init__(self, db):
         #connect to db
         self.con = sqlite3.connect(db)
@@ -9,10 +11,14 @@ class database(object):
         #crusor object allow people to execute commands
         self.cur = self.con.cursor()
 
-    def insStudent(self, ID, first, last, year, major=None, minor=None):
-        data = [ID, first, last, year, major, minor]
+    #method to add students
+    def insStudent(self, ID, first, last, year):
+        #data holder
+        data = [ID, first, last, year]
+        #execute to update data
         try:
-            self.cur.execute('insert into students values(?,?,?,?,?,?)', data)
+            #proccess data and insert new data into the database
+            self.cur.execute('insert into students values(?,?,?,?)', data)
             #commit the new name to the database
             self.con.commit()
         except sqlite3.IntegrityError, value:
@@ -23,14 +29,17 @@ class database(object):
                     UNIQUE : duplication of primary key
                     NOT NULL: when input a None into the input of a none null attribute
             '''
-            #rollback to earlier commit
+            #Unsuccessful update, rollback to earlier commit
             self.con.rollback()
 
-    def delStudent(ID):
-        data = ID
+    def delStudent(self, ID):
+        data = [ID]
         try:
-            self.cur.execute('delete from students ')
-
+            self.cur.execute('delete from students where ID = (?)', data)
+            self.con.commit()
+        except sqlite3.IntegrityError, value:
+            logging.warning(value)
+            self.con.rollback()
     #method to close the connection with database
     def close(self):
         self.con.close()
@@ -38,7 +47,8 @@ class database(object):
 #main method
 def main():
     db = database('database/cup.db')
-    db.insStudent('tanguyen17', 'Tu', 'Nguyen', 2017, 'Math', 'Comp. Sci.')
+    #db.insStudent('tanguyen17', 'Tu', 'Nguyen', 2017)
+    #db.delStudent('tanguyen17')
     db.cur.execute('select * from students')
 
     for i in db.cur:
