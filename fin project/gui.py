91f05e5bf9fdfeff4ab63b29b7639020779ef3ac
tunguyen17 <mcassist@M1611118.local>
2016-11-10 22:40:19 -0500

    fix something

--- fin project/gui.py
+++ fin project/gui.py
@@ -13,31 +13,40 @@ class Gui:
         tk.Grid.rowconfigure(self.root, 0, weight=1)
         tk.Grid.columnconfigure(self.root, 0, weight=1)
 
+    def draw_table(self, db, r):
 
-        list_columns = ["ID", "First", "Last", "Year"]
+        list_columns = [a[1] for a in db.meta[r]]
+
+        ###  TREEVIEW INITIALIZATION AND CONFIGURATIONS  ###
         #create a tree view
         self.tree = ttk.Treeview(self.root)
-        #index the column
-        self.tree["columns"] = list_columns
-
-        self.tree.column("#0", width=50)
-        self.tree.heading("#0", text="No.")
-
-        for i in list_columns:
-            self.tree.column(i, width=100 )
-            self.tree.heading(i, text=i)
-
 
         #Scrollbars
         self.ysb = ttk.Scrollbar(self.root, orient='vertical', command=self.tree.yview)
         self.xsb = ttk.Scrollbar(self.root, orient='horizontal', command=self.tree.xview)
         self.tree.configure(yscroll=self.ysb.set, xscroll=self.xsb.set)
 
-
         #make the table appears in grid
         self.tree.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W,columnspan=8)
         self.ysb.grid(row=1, column=9, sticky=tk.N + tk.S + tk.E + tk.W)
         self.xsb.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W, columnspan=8)
+
+        ###  DATA  ###
+
+        #index the column
+        self.tree["columns"] = list_columns
+
+        ## column name ##
+        #row ndex
+        self.tree.column("#0", width=50)
+        self.tree.heading("#0", text="No.")
+        #columb attributes
+        for i in list_columns:
+            self.tree.column(i, width=100 )
+            self.tree.heading(i, text=i)
+
+
+
 
 
         #make the window appears
@@ -48,4 +57,7 @@ if __name__ == "__main__":
 
     #connecting with the database
     db = DB.Database('database/cup.db')
-    print [a[1] for a in db.meta['students']]
+
+    #gui.draw_table(db, 'students')
+
+    print db.fetchData('students')
